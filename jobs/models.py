1d6f9b7d849387475299812330c3cb78225bc30b
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-06 21:12:41 +0000

    Created Job model in jobs app

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Job(models.Model):
+    '''
+    Stores information about user's projects/jobs
+    that they worked on
+    '''
+    image = models.ImageField(uploaded_to='images/')
+    summary = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.summary
