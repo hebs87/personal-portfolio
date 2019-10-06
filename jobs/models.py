9dd4a3c8c564352691b09a31907009cec7e67552
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-06 21:20:19 +0000

    Amended upload_to attr in image field in Job model

--- jobs/models.py
+++ jobs/models.py
@@ -7,7 +7,7 @@ class Job(models.Model):
     Stores information about user's projects/jobs
     that they worked on
     '''
-    image = models.ImageField(uploaded_to='images/')
+    image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
 
     def __str__(self):
