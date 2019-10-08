6248a0f3759cdee157d74c0554ff696358b79c88
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-08 21:05:50 +0000

    Created blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class Blog(models.Model):
+    '''
+    Allows user to add a blog post
+    '''
+    title = models.CharField(
+        max_length=100,
+        blank=False)
+    pub_date = models.DateTimeField(
+        blank=False,
+        null=False,
+        auto_now_add=True)
+    body = models.TextField(
+        max_length=2000,
+        blank=False)
+    image = models.ImageField(
+        upload_to='images/',
+        blank=True,
+        null=True)
+
+    def __str__(self):
+        return self.title
