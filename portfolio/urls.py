4d57b2996ae26024e67f3dcb3ed30908a4134c16
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-08 21:18:08 +0000

    Added basic home view in jobs and added url in root urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from jobs.views import home as jobs_home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', jobs_home, name="home")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
