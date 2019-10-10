8c84909f43213c72fcfe3c4aab78656c04c184e9
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-10 21:04:28 +0000

    Included blog urls in root urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,5 +21,6 @@ from jobs.views import home as jobs_home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', jobs_home, name="home")
+    path('', jobs_home, name="home"),
+    path('blog/', include(blog.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
