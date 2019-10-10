80489e14e013176291239ecd3f9317a45ed4e5c7
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-10 21:00:49 +0000

    Corrected home view and injected data into job cards in home.html

--- jobs/views.py
+++ jobs/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from .models import Job
 
+
 # Create your views here.
 def home(request):
     '''
     Render the home page and inject Job model data
     into the HTML template
     '''
-    jobs = Job.objects
+    jobs = Job.objects.all()
 
     context = {
         "jobs": jobs,
