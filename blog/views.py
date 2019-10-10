53060431f5d6e370b4705f712c3060e7712d77f5
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-10 21:13:29 +0000

    Created basic all_blogs view to render all_blogs.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def all_blogs(request):
+    '''
+    Renders the all_blogs.html page and injects
+    data from Blog model into the page
+    '''
+    return render(request, "all_blogs.html")
