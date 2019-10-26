bcc522716039a7873c037e048906184063cab9b3
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-26 11:59:14 +0000

    Added blog_detail view to render full blog details for single blog

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 
@@ -15,3 +15,18 @@ def all_blogs(request):
     }
 
     return render(request, "all_blogs.html", context)
+
+
+def blog_detail(request, blog_id):
+    '''
+    Renders the blog_detail.html page and injects
+    all blog data into the page - blog_id in the url
+    so it needs to be in the relevant function too
+    '''
+    blog_detail = get_object_or_404(Blog, pk=blog_id)
+
+    context = {
+        'blog_detail': blog_detail,
+    }
+
+    return render(request, "blog_detail.html", context)
